6d176a4e123674d63a00a7fc8affcc58664cbd45
yunglleung1 <yungl.leung1@gmail.com>
2018-06-30 18:39:59 -0400

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,14 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative 'fancy_dance.rb'
+
+class Dancer
+  extend FancyDance::ClassMethods # extends from Nested Modules
+  include FancyDance::InstanceMethods # includes from Nested Modules
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
