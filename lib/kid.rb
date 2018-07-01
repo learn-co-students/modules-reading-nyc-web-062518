a9146b56c9ca6e173e2cc7e5868a1a9c71673c1d
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-06-30 23:01:45 -0400

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,29 @@
-require_relative './dance_module.rb'
-require_relative './class_methods_module.rb'
+# Without nesting our modules, we need to use:
+#
+# require_relative './dance_module.rb'
+# require_relative './class_methods_module.rb'
+#
+# class Kid
+#   extend MetaDancing
+#   include Dance
+#
+#   attr_accessor :name
+#
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+
+require_relative './fancy_dance.rb'
+
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
